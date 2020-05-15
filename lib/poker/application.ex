6a097ec5e7c166056a762290c8a5093b16a20e2a
Dmitry Dubina <dmitryd.prog@gmail.com>
2020-05-16 00:49:53 +0700

    Подготовка prod билда

--- lib/poker/application.ex
+++ lib/poker/application.ex
@@ -5,7 +5,6 @@ defmodule Poker.Application do
 
   def start(_type, _args) do
     children = [
-      PokerWeb.Telemetry,
       {Phoenix.PubSub, name: Poker.PubSub},
       PokerWeb.Endpoint,
       Poker.Coordinator
